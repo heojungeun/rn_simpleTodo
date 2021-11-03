58aa3f976183a9e0078156010b5ff728849de370
heojungeun <heocube@naver.com>
2021-11-03 23:30:04 +0900

    update touchable

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  TouchableHighlight, 
+  TouchableOpacity, View, 
+  TouchableWithoutFeedback ,
+  Pressable,
+} from 'react-native';
+import { theme } from './colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity><Text style={styles.btnText}>Work</Text></TouchableOpacity>
+        <Pressable onPress={() => console.log("touch")}>
+          <Text style={styles.btnText}>Rest</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -14,8 +27,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 30,
   },
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingTop: 100,
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: "600"
+  }
 });
---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple todo app with rest
+
+### 투두 리스트🧑🏻‍💻
+- [ ] 1차 투두 앱 완성
+- [ ] 일과 휴식 디자인 구분되게 만들기
+- [ ] ...etc
+
+##### 참조 🧑🏻‍🎤
++ 리액트 네이티브 Document: [reactnative.dev](https://reactnative.dev/docs/components-and-apis)
++ Expo API Reference: [docs.expo.dev](https://docs.expo.dev/versions/latest/)
